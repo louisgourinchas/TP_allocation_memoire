2fae1166a3bdf3dc1a9e744e47a892e4dcaace82
louisgourinchas <louis.gourinchas@gmail.com>
2022-09-23 11:13:47 +0200

    mem_alloc et mem_free + test

--- src/mem.c
+++ src/mem.c
@@ -24,13 +24,12 @@ typedef struct mem_state{
 	void *adresse;
 	size_t size;
 	struct mem_free_block_s* first;
+	struct mem_fit_function_t function;
 } mem_state_t;
 
 //typedef struct mem_state mem_state_t;
 
 static mem_state_t gbl_state;
-//vide.
-//static mem_fit_function_t gbl_function;
 
 //-------------------------------------------------------------
 // mem_init
@@ -45,6 +44,7 @@ void mem_init() {
 	gbl_state.first = gbl_state.adresse;
 	gbl_state.first->size=gbl_state.size;
 	gbl_state.first->next=NULL;
+	mem_set_fit_handler(mem_first_fit);
 }
 
 //-------------------------------------------------------------
@@ -55,18 +55,16 @@ void mem_init() {
 **/
 void *mem_alloc(size_t size) {
 
+
 	//Possiblement nécessaire de changer la taille pour prendre en compte l'en-tête (enlevé pour simplifier)
 	//Problèmesi on n'accompte pas pour l'en-tete: l'user 
-	/*
 	size_t realsize = size;
 	if(size%8 != 0){
-		realsize = 8*((size/8)+2); //+1 pour avoir le multiple de 8 supp, +2 pour compter la taille de l'entete
-	}*/
-
-	//on essaie d'allouer un block plus grand que la mémoire
-	if(size > gbl_state.size){
-		return NULL;
+		realsize = 8*((size/8)+1); //+1 pour avoir le multiple de 8 supp
 	}
+	realsize+=sizeof(struct mem_free_block_s);
+
+	struct mem_free_block_s * emplacement = gbl_function(gbl_state.first, realsize);
 
 	//on trouve un block de taille suffisante
 	struct mem_free_block_s *block_courant = gbl_state.first; 	
@@ -84,43 +82,37 @@ void *mem_alloc(size_t size) {
 		return NULL;
 	}
 
-	if (block_precedent == NULL){ //le premier block libre était de taille suffisante.
+	if (block_courant -> size == realsize){ //on alloue le block courant en entier.
 
-		//on a alloué exactement toute la mémoire disponible 
-		if (gbl_state.first->size == size){
-
-			gbl_state.first = NULL;
-
-		} else { //il reste de la mémoire libre dans gbl_state.first
-
-			size_t new_size = (gbl_state.first->size)-size;
-			struct mem_free_block_s *new_next = gbl_state.first->next;
-			gbl_state.first = gbl_state.first+size;
-			gbl_state.first->size = new_size;
-			gbl_state.first->next = new_next;
-
-		}
-		block_alloue = (void *) block_courant;
-		block_alloue->size = size;
-
-	} else {	//le premier block n'était pas suffisant, on est sur un block b quelquonque dans la mémoire.
-
-		if (block_courant -> size == size){ //on alloue le block courant en entier.
-
+		if(block_precedent != NULL)
+		{
 			block_precedent->next = block_courant->next;
-
-		} else { //il reste de la mémoire libre dans block_courant
-
-			struct mem_free_block_s *residus = block_courant+size;
-			residus->size = block_courant->size - size;
-			residus->next = block_courant->next;
-			block_precedent->next = residus;
 		}
-		block_alloue = (void *) block_courant;
-		block_alloue->size = size;
-	}
+		else
+		{
+			gbl_state.first = block_courant->next;
+		}
+		
 
-	return (void *)block_alloue + 8;
+	} else { //il reste de la mémoire libre dans block_courant
+
+		struct mem_free_block_s *block_reste = (void*)block_courant+realsize;
+		block_reste->size = block_courant->size - realsize;
+		block_reste->next = block_courant->next;
+
+		if (block_precedent != NULL)
+		{
+			block_precedent->next = block_reste;
+		}
+		else
+		{
+			gbl_state.first = block_reste;
+		}	
+	}
+	block_alloue = (void *) block_courant;
+	block_alloue->size = realsize;
+
+	return block_alloue + 1;
 }
 
 //-------------------------------------------------------------
@@ -133,6 +125,7 @@ size_t mem_get_size(void * zone)
     return block_zone->size;
 }
 
+
 //-------------------------------------------------------------
 // mem_free
 //-------------------------------------------------------------
@@ -140,35 +133,98 @@ size_t mem_get_size(void * zone)
  * Free an allocaetd bloc.
 **/
 void mem_free(void *zone) {
+
+		
     //TODO: implement
 	struct mem_alloue_block_s* block_zone = zone-8;
+
+	if(gbl_state.first == NULL)
+	{
+		struct mem_free_block_s* block_zone_lible = (void *)block_zone;
+		block_zone_lible->size = block_zone->size;
+		block_zone_lible->next = NULL;
+		gbl_state.first=block_zone_lible;
+	}
+
 	struct mem_free_block_s* block_prec = gbl_state.first;
 	struct mem_free_block_s* block_suiv = block_prec->next;
 
 	//cas où la zone est placée avant le premier block (gbl_state.first OU block_prec ici).
-	if (block_zone < block_prec){
+	if ((void *)block_zone < (void *)block_prec){
 
-		if (block_zone + block_zone->size == block_prec){ //la zone est "collée" au premier block.
+		if ((void * )(block_zone + block_zone->size) == (void *)block_prec){ //la zone est "collée" au premier block.
 
-			//Appel Fonction mem_fuse avec block_zone et block_prec.
+			size_t sauvegarde_taille = block_prec->size;
+			block_prec = (void *)block_zone;
+			block_prec->size = block_zone->size + sauvegarde_taille;
+			block_prec->next = block_suiv;
+			gbl_state.first = block_prec;
 
 		} else { //il existe un ou plusieurs block(s) alloués entre la zone et block_prec.
-			struct mem_free_block_s* block_zone_libere = (void *) block_alloue;
-			block_zone_libere.size = block_alloue->size;
-			block_zone_libere.next = gbl_state.first; //Plus propre/logique d'utiliser block_prec ?
+			struct mem_free_block_s* block_zone_libere = (void *) block_zone;
+			block_zone_libere->size = block_zone->size;
+			block_zone_libere->next = gbl_state.first; //Plus propre/logique d'utiliser block_prec ?
 			gbl_state.first = block_zone_libere;
 		}
-
 		return; //pas super elegant, safe, à enlever si on est sur de ne pas rentrer dans les autres conditionelles.
 	}
-
-	//On cherche à se placer tel que block_prec < block_courant <blokc_suiv.
+	
+	//On cherche à se placer tel que block_prec < block_zone < block_suiv.
 	//on n'accompte pas pour la possibilité que l'utilisateur appelle free sur une zone libre.
-	While(block_prec < block_zone){
+	while(block_suiv != NULL && (void *)block_suiv < (void *)block_zone){
 		block_prec = block_suiv;
 		block_suiv = block_prec->next;
-	}
+	} 
 
+	if((void *)block_prec < (void *)block_zone && (void *)block_zone < (void *)block_suiv){
+		// il y a block allouee entre block zone et block prec  ET  entre block zone et block suiv
+		if ((void *)(block_prec + block_prec->size) < (void *)block_zone && (void *)(block_zone + block_zone->size) < (void *)block_suiv)
+		{
+			struct mem_free_block_s* block_zone_lible = (void *)block_zone;
+			block_prec->next = block_zone_lible;
+			block_zone_lible->next = block_suiv;
+			block_zone_lible->size = block_zone->size;
+		}
+		// il y a block allouee entre block zone et block suiv  ET  block zone est justement colle apres block prec 
+		else if ((void *)(block_prec + block_prec->size) == (void *)block_zone && (void *)(block_zone + block_zone->size) < (void *)block_suiv)
+		{
+			block_prec->size += block_zone->size;
+		}
+		// il y a block allouee entre block zone et block prec  ET  block zone est justement colle avant block suiv
+		else if ((void *)(block_prec + block_prec->size) < (void *)block_zone && (void *)(block_zone + block_zone->size) == (void *)block_suiv)
+		{
+			struct mem_free_block_s* sauvegarde_suivant = block_suiv;
+			block_suiv = (void *)block_zone;
+			block_suiv->next = sauvegarde_suivant->next;
+			block_prec->next = block_suiv;
+			block_suiv->size = block_zone->size + sauvegarde_suivant->size;
+		}
+		// block zone colle apres block prec et avant block suiv
+		else if ((void *)(block_prec + block_prec->size) == (void *)block_zone && (void *)(block_zone + block_zone->size) == (void *)block_suiv)
+		{
+			block_prec->size += block_zone->size + block_suiv->size;
+			block_prec->next = block_suiv->next;
+		}	
+	}
+	// zone est placé après le dernier block libre
+	else
+	{
+		// il y a block allouee entre block zone et block prec
+		if ((void *)(block_prec + block_prec->size) < (void *)block_zone)
+		{
+			struct mem_free_block_s* block_zone_lible = (void *)block_zone;
+			block_zone_lible->size = block_zone->size;
+			block_prec->next = block_zone_lible;
+			block_zone_lible->next = NULL;
+		}
+		//block zone colle apres block prec
+		else
+		{
+			block_prec->size += block_zone->size;
+		}
+		
+	}
+	
 	//Deux cas: 
 	//	-on a trouvé la situation où prec<zone<suiv
 	//	-on ne l'a pas trouvé: zone est placé après le dernier block libre.
@@ -187,7 +243,7 @@ void mem_show(void (*print)(void *, size_t, int free)) {
 	struct mem_alloue_block_s *block_courant_alloue;
 
 	//On continue tant qu'on n'a pas parcouru tout le block mémoire.
-	while (adresse_test != gbl_state.adresse + gbl_state.size){
+	while (adresse_test <= gbl_state.adresse + gbl_state.size){
 
 		//soit notre pointeur adresse_test est sur un block alloué soit sur un block libre (ou sur la fin de la mémoire, mais on ne passe pas dans la boucle).
 		//donc seulement 2 cas possibles à traiter.
@@ -202,7 +258,7 @@ void mem_show(void (*print)(void *, size_t, int free)) {
 				block_courant_libre = block_courant_libre->next;
 			} else {	//le prochain block est un block alloue
 				adresse_test = block_courant_libre->next;
-				block_courant_alloue = (void *) block_courant_libre->next;
+				block_courant_alloue = (void *) block_courant_libre + block_courant_libre->size;
 			}
 		} else { //le pointeur n'est pas sur un block libre, donc block alloue
 			block_courant_alloue = adresse_test;
@@ -220,17 +276,37 @@ void mem_show(void (*print)(void *, size_t, int free)) {
 // mem_fit
 //-------------------------------------------------------------
 void mem_set_fit_handler(mem_fit_function_t *mff) {
-	//TODO: implement
-	assert(! "NOT IMPLEMENTED !");
+	gbl_state.function = mff;
 }
 
 //-------------------------------------------------------------
 // Stratégies d'allocation
 //-------------------------------------------------------------
 mem_free_block_t *mem_first_fit(mem_free_block_t *first_free_block, size_t wanted_size) {
-    //TODO: implement
-	assert(! "NOT IMPLEMENTED !");
-	return NULL;
+    
+	//on essaie d'allouer un block plus grand que la mémoire
+	if(wanted_size > gbl_state.size){
+		return NULL;
+	}
+
+	//on trouve un block de taille suffisante
+	struct mem_free_block_s *block_courant = first_free_block; 	
+	struct mem_free_block_s *block_precedent = NULL;
+	struct mem_alloue_block_s *block_alloue;
+
+	//parcours des blocks vides jusqu'a arriver à la fin OU trouver un block courant de taille suffisante.
+	while(block_courant != NULL && block_courant->size < wanted_size){
+		block_precedent = block_courant;
+		block_courant = block_courant->next;
+	}
+
+	//pas de block mémoire de taille suffisante.
+	if (block_courant == NULL){
+		return NULL;
+	}else {
+		return block_courant;
+	}
+
 }
 //-------------------------------------------------------------
 mem_free_block_t *mem_best_fit(mem_free_block_t *first_free_block, size_t wanted_size) {
